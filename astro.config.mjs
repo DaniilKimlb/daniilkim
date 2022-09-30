70ee2cd96a7e7504e3e3c8a100af7ce84690a1f2
Daniil <dan.kim.04lb@gmail.com>
2022-09-30 23:54:27 +0600

    build/perf: updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 export default defineConfig({
-  integrations: [tailwind({ config: { applyBaseStyles: false }}), sitemap(), image(), compress({ img: false })],
-})
+  site: 'https://daniilkim.me/',
+  integrations: [
+		tailwind({ config: { applyBaseStyles: false } }),
+		sitemap(),
+		image({serviceEntryPoint: '@astrojs/image/sharp'}),
+		compress({ img: false }),
+	]
+}
+  );
