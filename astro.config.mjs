b7ea4dfc70934760fe1d3d403fd0fe19c5c4d033
Daniil <dan.kim.04lb@gmail.com>
2022-10-01 00:22:36 +0600

    feat: added prettier to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
-import compress from 'astro-compress';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import image from '@astrojs/image'
+import compress from 'astro-compress'
 
 export default defineConfig({
   site: 'https://daniilkim.me/',
   integrations: [
-		tailwind({ config: { applyBaseStyles: false } }),
-		sitemap(),
-		image({serviceEntryPoint: '@astrojs/image/sharp'}),
-		compress({ img: false }),
-	]
-}
-  );
+    tailwind({ config: { applyBaseStyles: false } }),
+    sitemap(),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+    compress({ img: false }),
+  ],
+})
---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  jsxBracketSameLine: true,
+  endOfLine: 'auto',
+  tailwindConfig: './tailwind.config.cjs',
+  plugins: [require('prettier-plugin-astro'), require('prettier-plugin-tailwindcss')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
