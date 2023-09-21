0c56b46e4ea935bc157f32aadefa66f6d3faea7e
daniil <dan.kim.04lb@gmail.com>
2023-09-21 20:17:42 +0600

    build(app): updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import image from '@astrojs/image'
 import compress from 'astro-compress'
 import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   site: 'https://daniilkim.me/',
+  output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
@@ -15,7 +15,6 @@ export default defineConfig({
   integrations: [
     tailwind({ config: { applyBaseStyles: false } }),
     sitemap(),
-    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
     compress({ img: false }),
   ],
 })
