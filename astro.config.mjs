39920304951ccdd17b5f14ddcaa96074d5f0ceba
daniil <dan.kim.04lb@gmail.com>
2023-09-21 18:52:35 +0600

    feat(app): added web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import image from '@astrojs/image'
 import compress from 'astro-compress'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   site: 'https://daniilkim.me/',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [
     tailwind({ config: { applyBaseStyles: false } }),
     sitemap(),
